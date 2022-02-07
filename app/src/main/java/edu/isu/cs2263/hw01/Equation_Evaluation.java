2f19a37a8798b4d1eae071e42864aecd4ed16c83
Michael <esqumich@isu.edu>
2022-02-06 18:08:27 -0700

    Committing evaluation feature

---
+++ app/src/main/java/edu/isu/cs2263/hw01/Equation_Evaluation.java
@@ -0,0 +1,269 @@
+package edu.isu.cs2263.hw01;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/***
+ * Class used to evaluate user equation.
+ * @author Michael Esquivias
+ */
+
+public class Equation_Evaluation implements Equation_Input {
+    Scanner input = new Scanner(System.in);
+
+    /***
+     * Asks for user input into terminal and converts input into array of characters.
+     * Parses through character array and adds element to one of two arraylists.
+     * If the character is a number, it gets added into one array list, operators go into the other.
+     * The 'operators' arraylist is parsed and based on the operator, the first two numbers in the 'numbers' list go through the appropriate arithmetic.
+     * The result of the first operation is stored into 'tempResult' and used for further arithmetic as the 'numbers' array gets smaller.
+     * @return A string showing the user equation plus the final answer.
+     */
+
+    public String getEquation() {
+
+        System.out.println("Please type in an equation: ");
+        String equation = input.nextLine();
+
+        char[] array = equation.toCharArray();
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        ArrayList<String> operators = new ArrayList<String>();
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == '0') {
+                numbers.add(0);
+            }
+            else if (array[i] == '1') {
+                numbers.add(1);
+            }
+            else if (array[i] == '2') {
+                numbers.add(2);
+            }
+            else if (array[i] == '3') {
+                numbers.add(3);
+            }
+            else if (array[i] == '4') {
+                numbers.add(4);
+            }
+            else if (array[i] == '5') {
+                numbers.add(5);
+            }
+            else if (array[i] == '6') {
+                numbers.add(6);
+            }
+            else if (array[i] == '7') {
+                numbers.add(7);
+            }
+            else if (array[i] == '8') {
+                numbers.add(8);
+            }
+            else if (array[i] == '9') {
+                numbers.add(9);
+            }
+            else if (array[i] == '+') {
+                operators.add("+");
+            }
+            else if (array[i] == '-') {
+                operators.add("-");
+            }
+            else if (array[i] == '/') {
+                operators.add("/");
+            }
+            else if (array[i] == '*') {
+                operators.add("*");
+            }
+
+        }
+
+        int firstIndex = 0;
+        float firstNum;
+        float secondNum;
+        int endResult = 0;
+        float tempResult = 0;
+
+        for (var operator : operators) {
+
+            if (operator == "+") {
+
+                if(tempResult != 0) {
+                    firstNum = tempResult;
+
+                }
+
+                else {
+                    firstNum = numbers.remove(firstIndex);
+
+                }
+                secondNum = numbers.remove(firstIndex);
+                tempResult = firstNum + secondNum;
+
+            }
+
+            else if (operator == "-") {
+
+                if(tempResult != 0) {
+                    firstNum = tempResult;
+
+                }
+
+                else {
+                    firstNum = numbers.remove(firstIndex);
+
+                }
+                secondNum = numbers.remove(firstIndex);
+                tempResult = firstNum - secondNum;
+            }
+
+            else if (operator == "/") {
+
+                if(tempResult != 0) {
+                    firstNum = tempResult;
+
+                }
+
+                else {
+                    firstNum = numbers.remove(firstIndex);
+
+                }
+                secondNum = numbers.remove(firstIndex);
+                tempResult = firstNum / secondNum;
+            }
+
+            else if (operator == "*") {
+
+                if(tempResult != 0) {
+                    firstNum = tempResult;
+
+                }
+
+                else {
+                    firstNum = numbers.remove(firstIndex);
+
+                }
+                secondNum = numbers.remove(firstIndex);
+                tempResult = firstNum * secondNum;
+            }
+
+            endResult = Math.round(tempResult);
+
+        }
+
+        System.out.println("Here is your equation and answer: ");
+        System.out.println();
+        return equation + " = " + endResult;
+    }
+
+    public void getEquation2() throws FileNotFoundException {
+        Scanner scanner;
+        scanner = new Scanner(new File("src/main/java/edu/isu/cs2263/hw01/test.txt"));
+
+        while(scanner.hasNext()) {
+            String str = scanner.nextLine();
+            char[] array = str.toCharArray();
+
+            ArrayList<Integer> numbers = new ArrayList<Integer>();
+            ArrayList<String> operators = new ArrayList<String>();
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == '0') {
+                    numbers.add(0);
+                } else if (array[i] == '1') {
+                    numbers.add(1);
+                } else if (array[i] == '2') {
+                    numbers.add(2);
+                } else if (array[i] == '3') {
+                    numbers.add(3);
+                } else if (array[i] == '4') {
+                    numbers.add(4);
+                } else if (array[i] == '5') {
+                    numbers.add(5);
+                } else if (array[i] == '6') {
+                    numbers.add(6);
+                } else if (array[i] == '7') {
+                    numbers.add(7);
+                } else if (array[i] == '8') {
+                    numbers.add(8);
+                } else if (array[i] == '9') {
+                    numbers.add(9);
+                } else if (array[i] == '+') {
+                    operators.add("+");
+                } else if (array[i] == '-') {
+                    operators.add("-");
+                } else if (array[i] == '/') {
+                    operators.add("/");
+                } else if (array[i] == '*') {
+                    operators.add("*");
+                }
+
+            }
+
+            int firstIndex = 0;
+            float firstNum;
+            float secondNum;
+            int endResult = 0;
+            float tempResult = 0;
+
+            for (var operator : operators) {
+
+                if (operator == "+") {
+
+                    if (tempResult != 0) {
+                        firstNum = tempResult;
+
+                    } else {
+                        firstNum = numbers.remove(firstIndex);
+
+                    }
+                    secondNum = numbers.remove(firstIndex);
+                    tempResult = firstNum + secondNum;
+
+                } else if (operator == "-") {
+
+                    if (tempResult != 0) {
+                        firstNum = tempResult;
+
+                    } else {
+                        firstNum = numbers.remove(firstIndex);
+
+                    }
+                    secondNum = numbers.remove(firstIndex);
+                    tempResult = firstNum - secondNum;
+                } else if (operator == "/") {
+
+                    if (tempResult != 0) {
+                        firstNum = tempResult;
+
+                    } else {
+                        firstNum = numbers.remove(firstIndex);
+
+                    }
+                    secondNum = numbers.remove(firstIndex);
+                    tempResult = firstNum / secondNum;
+                } else if (operator == "*") {
+
+                    if (tempResult != 0) {
+                        firstNum = tempResult;
+
+                    } else {
+                        firstNum = numbers.remove(firstIndex);
+
+                    }
+                    secondNum = numbers.remove(firstIndex);
+                    tempResult = firstNum * secondNum;
+                }
+
+                endResult = Math.round(tempResult);
+
+            }
+
+            System.out.println();
+            System.out.println("Here is your equation and answer: ");
+            System.out.println();
+            System.out.println(str + " = " + endResult);
+        }
+
+    }
+}
---
+++ app/src/main/java/edu/isu/cs2263/hw01/Equation_Input.java
@@ -0,0 +1,18 @@
+package edu.isu.cs2263.hw01;
+
+/***
+ * Interface used for accepting input.
+ * @author Michael Esquivias
+ */
+
+public interface Equation_Input {
+
+    /***
+     * Backbone for getting user equation, parsing it, and returning results to user.
+     * @return A string showing the user equation plus the answer.
+     */
+    String getEquation();
+
+}
+
+
